31f21766779685c5edc913a8a9762d0f0e30e2da
RvanB <rvanbronkhorst@gmail.com>
2022-03-15 20:38:10 -0700

    Progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,30 +5,49 @@ import './App.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Navbar className="topbar" bg="light" variant="light" expand="sm" sticky="top">
-          <Container fluid>
+        <Navbar className="topbar" bg="light" variant="light" expand="md" sticky="top">
+          <Container>
             <Navbar.Brand href="/">Raiden van Bronkhorst</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/code">Code</Link>
+                <Link className="nav-link" to="/notcode">Not code</Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
         <Container>
-          <br />
-          <Outlet />
+          <Row>
+            <Col md="auto">
+              <div className="margin-info">
+                <Image className="portrait" src="/img/raiden.jpg" width="150ch" />
+                <ul className="socials">
+                  <li><i className="fa-solid fa-envelope"></i> <a href="mailto:rvanbronkhorst@gmail.com">Email</a></li>
+                  <li><i className="fa-brands fa-linkedin"></i> <a href="https://www.linkedin.com/in/rvanbronkhorst">LinkedIn</a></li>
+                  <li><i className="fa-brands fa-github"></i> <a href="https://github.com/RvanB">GitHub</a></li>
+                </ul>
+              </div>
+            </Col>
+            <Col md={true}>
+              <Outlet />
+
+              <footer>
+                <p>© 2022 Raiden van Bronkhorst</p>
+              </footer>
+
+            </Col>
+          </Row>
         </Container>
-        <footer>
-          <p>© 2022 Raiden van Bronkhorst</p>
-        </footer>
       </div>
     );
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import './Home.css';
 
+import Image from 'react-bootstrap/Image';
+
 class Home extends React.Component {
 
   render() { 
     return (
       <div className="Home">
+        <h4>About Me</h4>
+        <p>I'm Raiden, a Software Engineer in the San Francisco Bay Area. My primary interests are in computer graphics, computer vision, and machine learning, but I'm constantly picking up new interests. During my undergrad at Western Washington University, I worked on bioinformatics research under <a href="https://facultyweb.cs.wwu.edu/~jagodzf/">Dr. Filip Jagodzinski</a>, and computer vision research under <a href="https://facultyweb.cs.wwu.edu/~wehrwes/">Dr. Scott Wehrwein</a>. Outside of work I enjoy photography, lettering, hiking, music listening, and drinking tea.</p>
+        <h4>Resume</h4>
+        <iframe title="resume" src="/files/vanBronkhorstR_resume.pdf" width="100%" height="500"/>
       </div>
     );
   }
